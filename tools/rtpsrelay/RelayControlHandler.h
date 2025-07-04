b794b33fa65db590c2001f83fe71e988105a67eb
Lakshmanan <Prabhu.Lakshmanan@lennox.com>
2025-07-04 17:18:11 +0530

    param_value un-assigned changed

--- tools/rtpsrelay/RelayControlHandler.h
+++ tools/rtpsrelay/RelayControlHandler.h
@@ -15,19 +15,15 @@ public:
 
   void on_data_available(DDS::DataReader_ptr reader) override;
   void on_requested_deadline_missed(DDS::DataReader_ptr, const DDS::RequestedDeadlineMissedStatus&) override {}
-  void on_requested_incompatible_qos(DDS::DataReader_ptr, const DDS::RequestedIncompatibleQosStatus&) override {}
-  void on_liveliness_changed(DDS::DataReader_ptr, const DDS::LivelinessChangedStatus&) override {}
-  void on_subscription_matched(DDS::DataReader_ptr, const DDS::SubscriptionMatchedStatus&) override {}
-  void on_sample_lost(DDS::DataReader_ptr, const DDS::SampleLostStatus&) override {}
-  void on_sample_rejected(DDS::DataReader_ptr, const DDS::SampleRejectedStatus&) override {}
 
 private:
-  void process_command(const std::string& command, unsigned long parameter);
+  // Change the parameter type from unsigned long to ParameterValue
+  void process_command(const std::string& command, ParameterValue parameter);
 
   std::string relay_id_;
   DrainManager& drain_manager_;
 };
 
-} 
+}
 
-#endif 
+#endif
