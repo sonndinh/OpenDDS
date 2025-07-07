68775b0cab4348535c3816ccd7537ebce19f5bca
Lakshmanan <Prabhu.Lakshmanan@lennox.com>
2025-07-07 12:45:54 +0530

    resolving build error

--- tools/rtpsrelay/RelayStatusReporter.h
+++ tools/rtpsrelay/RelayStatusReporter.h
@@ -17,18 +17,17 @@ class RelayStatusReporter : public ACE_Event_Handler {
 public:
   RelayStatusReporter(const Config& config,
                       const GuidAddrSet& guid_addr_set,
-                      RelayStatusDataWriter_var status_writer,
+                      RelayStatusDataWriter_var writer,
                       ACE_Reactor* reactor,
-                      DrainManager* drain_manager = nullptr);
+                      DrainManager* drain_manager);
 
-private:
-  int handle_timeout(const ACE_Time_Value& now, const void* token) override;
-
-  const Config& config_;
+  int handle_timeout(const ACE_Time_Value& now, const void* act);
   void report_relay_status();
 
+private:
+  const Config& config_;
   const GuidAddrSet& guid_addr_set_;
-  RelayStatusDataWriter_var status_writer_;
+  RelayStatusDataWriter_var writer_;
   ACE_Reactor* reactor_;
   DrainManager* drain_manager_;
 };
