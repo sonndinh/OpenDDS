e607566716a843f337c22d87f7549e506b72462b
Son Dinh <dinhs@objectcomputing.com>
2026-05-04 11:45:43 -0500

    Use a global regex object instead of creating an object for each client

--- tools/rtpsrelay/RtpsRelay.cpp
+++ tools/rtpsrelay/RtpsRelay.cpp
@@ -634,8 +634,16 @@ int run(int argc, ACE_TCHAR* argv[])
   reactor_task->job_queue(TheServiceParticipant->job_queue());
   reactor_task->init_reactor_task(&TheServiceParticipant->get_thread_status_manager(), "RtpsRelay Main", reactor);
 
-  const auto guid_addr_set = make_rch<GuidAddrSet>(config, reactor_task, rtps_discovery,
-                                                   ref(relay_participant_status_reporter), ref(relay_statistics_reporter), ref(*relay_thread_monitor));
+  RcHandle<GuidAddrSet> guid_addr_set;
+  try {
+    guid_addr_set = make_rch<GuidAddrSet>(config, reactor_task, rtps_discovery, ref(relay_participant_status_reporter),
+                                          ref(relay_statistics_reporter), ref(*relay_thread_monitor));
+  } catch (const std::regex_error& e) {
+    ACE_ERROR((LM_ERROR, "(%P|%t) ERROR: exception std::regex_error for pattern '%C': %C\n",
+      config.certificate_id_pattern().c_str(), e.what()));
+    return EXIT_FAILURE;
+  }
+
   GuidPartitionTable guid_partition_table(config, reactor_task, spdp_horizontal_addr, *guid_addr_set, relay_partitions_writer, relay_statistics_reporter);
   RelayPartitionTable relay_partition_table(relay_statistics_reporter);
   relay_statistics_reporter.report();
